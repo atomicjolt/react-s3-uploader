117c359473d272d825da45483630d718449fb31c
JadenH <holladaystudio@gmail.com>
2015-06-09 11:25:15 -0600

    Remove get mime type from s3upload.

--- s3upload.js
+++ s3upload.js
@@ -60,7 +60,7 @@ S3Upload.prototype.createCORSRequest = function(method, url) {
 S3Upload.prototype.executeOnSignedUrl = function(file, callback) {
     var xhr = new XMLHttpRequest();
     var fileName = file.name.replace(/\s+/g, "_");
-    xhr.open('GET', this.signingUrl + '?objectName=' + fileName + '&mimeType=' + this.mimeType(fileName);, true);
+    xhr.open('GET', this.signingUrl + '?objectName=' + fileName, true);
     xhr.overrideMimeType && xhr.overrideMimeType('text/plain; charset=x-user-defined');
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
@@ -103,10 +103,9 @@ S3Upload.prototype.uploadToS3 = function(file, signResult) {
             }
         }.bind(this);
     }
-    var mimeType = signResult.mimeType || this.getMimeType(file.name);
-    xhr.setRequestHeader('Content-Type', mimeType);
-
     var acl = signResult.acl || 'public-read';
+
+    xhr.setRequestHeader('Content-Type', signResult.mimeType);
     xhr.setRequestHeader('x-amz-acl', acl);
 
     return xhr.send(file);
